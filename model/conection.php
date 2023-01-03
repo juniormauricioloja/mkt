47c65c5da4ee8eaa1c868f590a1c5a44a9a9dc08
juniormauricioloja <jrloja2495@gmail.com>
2023-01-03 15:51:00 -0500

    v4

--- model/conection.php
+++ model/conection.php
@@ -1,14 +1,14 @@
 <?php
-/*
+
 $host='localhost';
 $user2='gledsistema';
 $password2='gledcrm2022';
 $db='gled';
-*/
+/*
 $host='localhost';
 $user2='root';
 $password2='';
 $db='gled';
-
+*/
 
 $conection=mysqli_connect($host,$user2,$password2,$db);
